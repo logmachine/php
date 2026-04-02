71aede0eddd9b015d7837ca8c9eda17a3cc8fb1c
Elvis Otieno <125451537+the1Riddle@users.noreply.github.com>
2026-04-02 17:02:44 +0000

    to local test

--- logmachine-php/test.sh
+++
@@ -1,9 +0,0 @@
-curl -X POST "https://logmachine.bufferpunk.com/api/logs?room=php-logger" \
-  -H "Content-Type: application/json" \
-  -d '{
-    "user": "rezzcode",
-    "module": "logmachine-php",
-    "level": "INFO",
-    "timestamp": "2025-08-06T18:00:00+00:00",
-    "message": "Priming php-logger room"
-  }'
